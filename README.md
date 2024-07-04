99ff6f1c64b739a622d1b1f6bf6508ddad6c85ed
TupleCipher <muonprime@googlemail.com>
2024-07-04 20:55:35 +0100

    updated readme with endpoints

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # DisconnectedDev-Core-API
+
+A fastAPI api to serve as the basis of the backend for the Disconnected Dev site.
+
+API documentation is built in and can be reached from a running api at the following endpoints:
+
+> OpenAPI interactive Documentation (previously Swagger UI)
+
+```http
+http://127.0.0.1:8000/docs
+```
+
+> Alternative ReDoc Documentation
+
+```http
+http://127.0.0.1:8000/redoc
+```
+
+### API Endpoint Reference
+
+> Blog endpoints:
+
+```python
+# return a list of all published blog entries
+"/blogs/"
+
+# create a new blog entry
+"/blogs/new_post/"
+
+# update a draft blog entry
+"/blogs/update_post/{blog_id}"
+
+# publish a draft blog entry to make public
+"/blogs/pub_post/{blog_id}"
+
+# return a list of all published blog entries from a specific user
+"/blogs/users_posts/{user_id}"
+
+# return a specific blog entry
+"/blogs/post/{blog_id}"
+
+# remove a specific blog entry from the database
+"/blogs/remove/{blog_id}"
+```
+
+> User endpoints:
+
+```python
+# add a user to the database
+"/users/add/"
+
+# update an existing user in the database
+"/users/update/{user_id}"
+
+# remove a user from the database
+"/users/remove/{user_id}"
+```
--- dd-core-api/core_api.py
+++ dd-core-api/core_api.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from pydantic import basemodel
 
 # define an instance of the fastapi app class
 dd_api = FastAPI()
+
+
+class Blog(BaseModel):
+    bID: int
+    bTitle: str
+    bText: str
+    bFullText: str
+    bCount: int | none = none
+    bCreated: str
 
 
 @dd_api.get("/")
@@ -24,13 +34,13 @@ async def new_blog_post():
     return {"msg": "Create new draft blog post"}
 
 
-@dd_api.put("/blogs/update_post/")
+@dd_api.put("/blogs/update_post/{blog_id}")
 async def update_blog_post():
     return {"msg": "Create new draft blog post"}
 
 
-@dd_api.put("/blogs/pub_post/")
-async def publish_blog_post():
+@dd_api.put("/blogs/pub_post/{blog_id}")
+async def publish_blog_post(blog_id: int):
     return {"msg": "Your post titled:###, has been published"}
 
 
@@ -39,14 +49,14 @@ async def users_blog_posts():
     return {"msg": "list of users blog posts"}
 
 
-@dd_api.get("/blogs/post/")
-async def blog_post():
+@dd_api.get("/blogs/post/{blog_id}")
+async def blog_post(blog_id: int):
     # add user presence check for draft blog
     return {"msg": "Will return a specific blog entry"}
 
 
-@dd_api.delete("/blogs/remove/")
-async def blog_remove():
+@dd_api.delete("/blogs/remove/{blog_id}")
+async def blog_remove(blog_id: int):
     return {"msg": "Blog post deleted from the database!"}
 
 #################### user endpoints ####################
@@ -57,11 +67,11 @@ async def user_add():
     return {"msg": "The user has been added to the database"}
 
 
-@dd_api.post("/users/update/")
-async def user_add():
+@dd_api.post("/users/update/{user_id}")
+async def user_add(user_id: int):
     return {"msg": "The user has been updated"}
 
 
-@dd_api.put("/users/remove/")
-async def user_add():
+@dd_api.put("/users/remove/{user_id}")
+async def user_add(user_id: int):
     return {"msg": "The user has been removed from the database"}
